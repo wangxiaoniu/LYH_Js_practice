3a2d7b9cf954795b412b7a71c399b504025e477f
wangxiaoniu <13519870949@163.com>
2017-06-12 17:07:05 +0800

    34

---
+++ 34/base.js
@@ -0,0 +1,21 @@
+
+
+
+//函数式写法
+/*function $(id){
+	return document.getElementById(id);
+
+}*/
+
+//对象式
+var Base={
+	getId:function(id){
+		return document.getElementById(id);
+	},
+	getName:function(name){
+		return document.getElementsByName(name)
+	},
+	getTagName:function(tag){
+		return document.getElementsByTagName(tag);
+	}
+}
